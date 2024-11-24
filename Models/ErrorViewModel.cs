60e3c1fc13cfb0026b08acbc37a246b690a9bf0b
Humberto Vitalino <humbertodark@outlook.com>
2024-11-24 11:05:33 -0300

    refactor: ErrorViewModel

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -1,9 +1,8 @@
-namespace EmprestimoDeLivros.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
+namespace EmprestimoDeLivros.Models;
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
