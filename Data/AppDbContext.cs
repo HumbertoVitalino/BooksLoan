99720226478535454b3805b0534feca8505ff862
Humberto Vitalino <humbertodark@outlook.com>
2024-11-24 11:04:56 -0300

    feat: dbcontext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using EmprestimoDeLivros.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmprestimoDeLivros.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {        
+    }
+
+    public DbSet<EmprestimosModel> Emprestimos { get; set; }
+}
