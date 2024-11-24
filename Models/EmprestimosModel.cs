6f24f615b3fe7a89dfaadd7c2c0762cc688768e6
Humberto Vitalino <humbertodark@outlook.com>
2024-11-24 11:31:57 -0300

    refactor: emprestimo model

--- Models/EmprestimosModel.cs
+++ Models/EmprestimosModel.cs
@@ -6,7 +6,7 @@ public class EmprestimosModel
     public string Recebedor { get; set; }
     public string Fornecedor { get; set; }
     public string LivroEmprestado { get; set; }
-    public DateTime DataEmprestimo {  get; set; } = DateTime.Now;
+    public DateTime DataUltimaAtualizacao {  get; set; } = DateTime.Now;
 }
 
 
