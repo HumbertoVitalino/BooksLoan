972bb7578b5179ecb2058c0debd4df3dae737af0
Humberto Vitalino <humbertodark@outlook.com>
2024-11-25 14:55:57 -0300

    refactor: emprestimosModel

--- Models/EmprestimosModel.cs
+++ Models/EmprestimosModel.cs
@@ -1,10 +1,17 @@
-﻿namespace EmprestimoDeLivros.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmprestimoDeLivros.Models;
 
 public class EmprestimosModel
 {
     public int Id { get; set; }
+    [Required(ErrorMessage = "Digite o recebedor")]
     public string Recebedor { get; set; }
+
+    [Required(ErrorMessage = "Digite o fornecedor")]
     public string Fornecedor { get; set; }
+
+    [Required(ErrorMessage = "Digite o livro emprestado")]
     public string LivroEmprestado { get; set; }
     public DateTime DataUltimaAtualizacao {  get; set; } = DateTime.Now;
 }
