51eeda1eca99a031a13fead7f592afe6f17bf3dc
Humberto Vitalino <humbertodark@outlook.com>
2024-11-25 08:58:17 -0300

    feat: emprestimos controller

---
+++ Controllers/EmprestimoController.cs
@@ -0,0 +1,19 @@
+﻿using EmprestimoDeLivros.Data;
+using EmprestimoDeLivros.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmprestimoDeLivros.Controllers;
+
+public class EmprestimoController : Controller
+{
+    private readonly AppDbContext _context;
+    public EmprestimoController(AppDbContext context)
+    {
+        _context = context;
+    }
+    public IActionResult Index()
+    {
+        IEnumerable<EmprestimosModel> emprestimos = _context.Emprestimos;
+        return View(emprestimos);
+    }
+}
