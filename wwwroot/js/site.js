a5affd274dcb97447542a2b76d400edfd1761129
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2024-12-02 21:40:36 -0300

    refactor: filter

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,30 @@
 ﻿$(document).ready(function () {
 
-    $('#Emprestimos').DataTable();
+    $('#Emprestimos').DataTable({
+        language: 
+        {
+            "decimal": "",
+            "emptyTable": "Não existem dados na tabela.",
+            "info": "Mostrando _START_ registro de _END_ em um total de _TOTAL_ entradas",
+            "infoEmpty": "Mostrando 0 até 0 de 0 entradas",
+            "infoFiltered": "(filtrado por _MAX_ total de entradas)",
+            "infoPostFix": "",
+            "thousands": ",",
+            "lengthMenu": "mostrar _MENU_ entradas",
+            "loadingRecords": "Loading...",
+            "processing": "",
+            "search": "Procurar:",
+            "zeroRecords": "Nenhum registro encontrado",
+            "paginate": {
+                "first": "Primeira",
+                "last": "Ultima",
+                "next": "Proxima",
+                "previous": "Anterior"
+            },
+            "aria": {
+                "orderable": "Ordernar por essa coluna",
+                "orderableReverse": "Inverter coluna"
+            }
+        }       
+    });
 });
