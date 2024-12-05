a2dcd85b17af069ae8440b8b58db7845d7aae082
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2024-12-04 21:26:18 -0300

    auto update

--- obj/Debug/net8.0/EmprestimoDeLivros.AssemblyInfo.cs
+++ obj/Debug/net8.0/EmprestimoDeLivros.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmprestimoDeLivros")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7367ef81cc9c1389917fb2055f9495c3b3b771ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+026b6dbef4876fbc2c1782d6f2bfbb7dbc5ccfe1")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmprestimoDeLivros")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmprestimoDeLivros")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
